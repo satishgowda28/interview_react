bf587b3ebcf96a4d528381c42682113f02d1c981
satishgowda28 <satishgowda28@gmail.com>
2025-12-08 09:15:31 +0530

    nested checkbox qn done

--- src/pages/nestedcheckbox/_components/checkboxLi.tsx
+++ src/pages/nestedcheckbox/_components/checkboxLi.tsx
@@ -1,19 +1,30 @@
-import type { FC, PropsWithChildren } from "react";
+import type { ChangeEvent, FC, PropsWithChildren } from "react";
 
 interface CheckboxType {
   label: string;
   id: number;
+  onChangeEvent: (e: ChangeEvent<HTMLInputElement>) => void;
+  checked: boolean;
 }
 const CheckboxLi: FC<PropsWithChildren<CheckboxType>> = ({
   label,
   id,
   children,
+  onChangeEvent,
+  checked,
 }) => {
   return (
     <li className={`relative`}>
       <div className="flex space-x-2 bg-[#242424] relative z-10">
-        <input type="checkbox" id={`${label}-${id}`} />
-        <label htmlFor={`${label}-${id}`}>{label}</label>
+        <input
+          type="checkbox"
+          id={`${label}-${id}`}
+          onChange={onChangeEvent}
+          checked={checked}
+        />
+        <label htmlFor={`${label}-${id}`}>
+          {id} - {label}
+        </label>
       </div>
       {children && (
         <i className="absolute left-1.5 w-px top-0 bottom-0 bg-white/50 z-0" />
--- src/pages/nestedcheckbox/index.tsx
+++ src/pages/nestedcheckbox/index.tsx
@@ -1,4 +1,10 @@
-import { useEffect, useState, type FC, type PropsWithChildren } from "react";
+import {
+  useEffect,
+  useState,
+  type ChangeEvent,
+  type FC,
+  type PropsWithChildren,
+} from "react";
 import CheckboxLi from "./_components/checkboxLi";
 import WrapperElem from "./_components/wrapper";
 import checkboxData from "./_data/index.json";
@@ -11,6 +17,7 @@ interface WrapperComp {
   allData?: CheckboxData;
   isRoot: boolean;
   refId: number;
+  ioChangeEvent: (idx: number) => (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const CheckboxWrapperComp: FC<PropsWithChildren<WrapperComp>> = ({
@@ -19,10 +26,16 @@ const CheckboxWrapperComp: FC<PropsWithChildren<WrapperComp>> = ({
   allData,
   isRoot,
   refId,
+  ioChangeEvent,
 }) => {
   return (
     <WrapperElem isRoot={isRoot}>
-      <CheckboxLi id={refId} label={allData?.[refId].label || ""}>
+      <CheckboxLi
+        id={refId}
+        label={allData?.[refId].label || ""}
+        onChangeEvent={ioChangeEvent(refId)}
+        checked={allData?.[refId].checked || false}
+      >
         {parentChildRel?.[refId]?.map((idx) => (
           <CheckboxWrapperComp
             key={`${allData?.[refId].label}_${idx}`}
@@ -30,6 +43,7 @@ const CheckboxWrapperComp: FC<PropsWithChildren<WrapperComp>> = ({
             parentChildRel={parentChildRel}
             allData={allData}
             refId={idx}
+            ioChangeEvent={ioChangeEvent}
             isRoot={false}
           />
         ))}
@@ -46,11 +60,80 @@ const NestedCheckbox = () => {
     const { rootIds, parentChild, flatData } = runNormalize(
       checkboxData as Node[]
     );
-    console.log({ rootIds, parentChild, flatData });
     setRootIds(rootIds);
     setParentChildRel(parentChild);
     setAllData(flatData);
   }, []);
+  const handleChekboxChange = (idx: number) => {
+    return (e: ChangeEvent<HTMLInputElement>) => {
+      let tempAllData = { ...allData };
+      tempAllData[idx].checked = e.target.checked;
+      const modByChildren = checkChildren(idx, e.target.checked, tempAllData);
+      const modByParent = checkParent(idx, e.target.checked, {
+        ...tempAllData,
+        ...modByChildren,
+      });
+      tempAllData = { ...tempAllData, ...modByChildren, ...modByParent };
+      setAllData((prev) => ({ ...prev, ...tempAllData }));
+    };
+  };
+  const checkChildren = (
+    idx: number,
+    value: boolean,
+    tempObj: CheckboxData
+  ) => {
+    const childrens = parentChildRel[idx];
+    if (childrens) {
+      for (let childId of childrens) {
+        tempObj[childId].checked = value;
+        if (parentChildRel[childId]) {
+          tempObj = {
+            ...tempObj,
+            ...checkChildren(childId, value, { ...tempObj }),
+          };
+        }
+      }
+    } else {
+      return tempObj;
+    }
+    return tempObj;
+  };
+  const checkParent = (idx: number, value: boolean, tempObj: CheckboxData) => {
+    const parentId = tempObj[idx].parent;
+    debugger;
+    if (parentId) {
+      if (!value) {
+        tempObj[parentId].checked = value;
+        tempObj = {
+          ...tempObj,
+          ...checkParent(parentId, value, { ...tempObj }),
+        };
+      } else {
+        const childrens = parentChildRel[parentId];
+        if (childrens) {
+          let allChecked = true;
+          for (let child of childrens) {
+            if (child !== idx) {
+              if (!tempObj[child].checked) {
+                allChecked = false;
+                break;
+              }
+            }
+          }
+          if (allChecked) {
+            tempObj[parentId].checked = value;
+            tempObj = {
+              ...tempObj,
+              ...checkParent(parentId, value, { ...tempObj }),
+            };
+          }
+        }
+      }
+    } else {
+      return tempObj;
+    }
+    return tempObj;
+  };
   return (
     <div>
       {rootIds?.map((idx) => (
@@ -60,6 +143,7 @@ const NestedCheckbox = () => {
           parentChildRel={parentChildRel}
           allData={allData}
           refId={idx}
+          ioChangeEvent={handleChekboxChange}
           isRoot
         />
       ))}
